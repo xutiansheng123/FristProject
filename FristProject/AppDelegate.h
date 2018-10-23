8935cd7cafd8a9c628c3e4f3b6c280f650a1280c
徐天盛 <xutiansheng@xutianshengdemacbook-air.local>
2018-10-23 12:48:23 +0800

    Initial Commit

---
+++ FristProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FristProject
+//
+//  Created by 徐天盛 on 2018/10/23.
+//  Copyright © 2018 徐天盛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FristProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FristProject
+//
+//  Created by 徐天盛 on 2018/10/23.
+//  Copyright © 2018 徐天盛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
